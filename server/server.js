97f24b8877ce77054963307483b9145317e0781e
emb-navenai <emb-navenai@embitel.com>
2024-12-10 10:47:53 +0530

    prod port issue

--- server/server.js
+++ server/server.js
@@ -40,6 +40,7 @@ app.use((err, req, res, next) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
